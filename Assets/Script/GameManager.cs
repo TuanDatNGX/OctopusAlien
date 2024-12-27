0678b750765fc67a5107b34853427f694150b45e
Duy Chuong <duychuong0999@gmail.com>
2024-12-27 17:41:57 +0700

    up

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class GameManager : KBTemplate.Patterns.Singleton.Singleton<GameManager>
 {
+    private void Awake()
+    {
+        Application.targetFrameRate = 120;
+    }
+
     public Camera mainCamera;
     public HpBarController hpBar;
     public GameObject bloodAlien;
