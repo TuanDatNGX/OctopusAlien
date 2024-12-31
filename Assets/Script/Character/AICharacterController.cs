41b9909b934084b54541ea62e48251eb648912f5
Duy Chuong <duychuong0999@gmail.com>
2024-12-31 17:53:01 +0700

    up

--- Assets/Script/Character/AICharacterController.cs
+++ Assets/Script/Character/AICharacterController.cs
@@ -59,7 +59,7 @@ public class AICharacterController : CharacterBase
 
     public override void AffterDie(CharacterBase _octopus = null)
     {
-        _octopus.GetExp(currentExp * 5);
+        _octopus.GetExp(currentLevel * 5);
         gameObject.SetActive(false);
     }
 
